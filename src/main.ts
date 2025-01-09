547a46e53411996f93b303aba7a03991fd26f5cd
Matthew Fournier <matthew@halfhelix.com>
2025-01-09 12:56:27 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,22 @@ import { EnvironmentVariables } from './types'
  */
 export async function run(): Promise<void> {
   try {
-    const variables: string = core.getInput('variables')
-    const token: string = core.getInput('token')
-    const store: string = core.getInput('store')
-    const branch: string = core.getInput('branch')
+    const variables: string = core.getInput('variables', {
+      required: true,
+      trimWhitespace: true
+    })
+    const token: string = core.getInput('token', {
+      required: true,
+      trimWhitespace: true
+    })
+    const store: string = core.getInput('store', {
+      required: true,
+      trimWhitespace: true
+    })
+    const branch: string = core.getInput('branch', {
+      required: true,
+      trimWhitespace: true
+    })
 
     if (!variables || !token || !store) {
       throw new Error('Environment is not configured')
@@ -28,7 +40,6 @@ export async function run(): Promise<void> {
       throw new Error(`Envrionment variable <${branchThemeKey}> is not set`)
     }
 
-    core.debug(`BranchId: ${branchThemeId}`)
     core.setOutput('branchThemeId', branchThemeId)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
